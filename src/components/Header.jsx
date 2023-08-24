271dddc45e2b80a937da54386be6da16c3a52ecd
Izaak Dale <izaakdaledev@gmail.com>
2023-08-24 11:27:22 -0700

    added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router';
+
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  function pathMatchRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+
+  function styleMenuButton(route) {
+    return `cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+      pathMatchRoute(route) && 'text-black border-b-red-400'
+    }`;
+  }
+
+  return (
+    <div className='bg-white border-bottom-b shadow-sm sticky top-0 z-50'>
+      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+        <div>
+          <img
+            src='https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg'
+            alt='logo'
+            className='h-5 cursor-pointer'
+          />
+        </div>
+        <div>
+          <ul className='flex space-x-10'>
+            <li onClick={() => navigate('/')} className={styleMenuButton('/')}>
+              Home
+            </li>
+            <li
+              onClick={() => navigate('/offers')}
+              className={styleMenuButton('/offers')}
+            >
+              Offers
+            </li>
+            <li
+              onClick={() => navigate('/sign-in')}
+              className={styleMenuButton('/sign-in')}
+            >
+              Sign-In
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
