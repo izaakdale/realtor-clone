a592d37e57e915e776f175bb922f1bb4b7862f02
Izaak Dale <izaakdaledev@gmail.com>
2023-09-05 14:02:11 -0700

    added google oauth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 import { FcGoogle } from 'react-icons/fc';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { toast } from 'react-toastify';
+import { db } from '../auth/firebase';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { useNavigate } from 'react-router';
 
 export default function OAuth() {
+  const navigate = useNavigate();
+  async function onGoogleClick(e) {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const docRef = doc(db, 'users', user.uid);
+      const docSnap = await getDoc(docRef);
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      }
+      navigate('/');
+    } catch (error) {
+      toast.error('could not authorize with Google');
+      console.log(error);
+    }
+  }
+
   return (
     <button
+      type='button'
+      onClick={onGoogleClick}
       className='w-full flex items-center justify-center py-2 
     bg-blue-600 hover:bg-blue-700 active:bg-blue-800
     text-gray-200 hover:text-white font-semibold uppercase text-sm
