d27b6faf587883bc780298a43922a9c9a9fcc4b8
Izaak Dale <izaakdaledev@gmail.com>
2023-09-13 11:28:08 -0700

    add my listing to profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,20 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { doc, updateDoc } from 'firebase/firestore';
-import React, { useState } from 'react';
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import { toast } from 'react-toastify';
 import { db } from '../auth/firebase';
 import { FcHome } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 export default function Profile() {
   const auth = getAuth();
@@ -18,6 +27,8 @@ export default function Profile() {
   const { name, email } = formData;
 
   const [editActive, setEditActive] = useState(false);
+  const [listings, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   function onSignOutClick(e) {
     auth.signOut();
@@ -44,6 +55,37 @@ export default function Profile() {
       toast.error('error updating profile');
     }
   }
+
+  useEffect(() => {
+    async function fetchUserListings() {
+      const listingsRef = collection(db, 'listings');
+      const q = query(
+        listingsRef,
+        where(
+          'userRef',
+          '==',
+          auth.currentUser.uid,
+          orderBy('timestamp', 'desc')
+        )
+      );
+
+      const querySnap = await getDocs(q);
+      let listings = [];
+
+      querySnap.forEach((doc) => {
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings);
+
+      console.log(listings);
+
+      setLoading(false);
+    }
+    fetchUserListings();
+  }, [auth.currentUser.uid]);
 
   return (
     <>
@@ -100,6 +142,24 @@ export default function Profile() {
           </Link>
         </div>
       </section>
+      <div className='max-w-6xl px-3 mt-6 mx-auto'>
+        {!loading && listings.length > 0 && (
+          <>
+            <h2 className='text-2xl text-center font-semibold'>My Listings</h2>
+            <ul>
+              {listings.map((listing) => {
+                return (
+                  <ListingItem
+                    key={listing.id}
+                    id={listing.id}
+                    listing={listing.data}
+                  />
+                );
+              })}
+            </ul>
+          </>
+        )}
+      </div>
     </>
   );
 }
